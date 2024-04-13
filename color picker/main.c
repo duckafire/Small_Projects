b01ffcac419600265023fff1b547d24d79b97209
DuckAfire <155199080+DuckAfire@users.noreply.github.com>
2024-04-13 11:00:57 -0300

    Update color viewer

--- color picker/main.c
+++ color picker/main.c
@@ -12,26 +12,57 @@ int main(int argc, char *argv[]){
 	window   = SDL_CreateWindow("Color Picker", (desktop.w - SCREEN_DIM) / 2, (desktop.h - SCREEN_DIM) / 2, SCREEN_DIM, SCREEN_DIM, 0);
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-	SDL_SetRenderDrawColor(renderer, 25, 25, 25, 255);
+	SDL_SetRenderDrawColor(renderer, 10, 10, 10, 255);
 	SDL_RenderClear(renderer);
 	
 	unsigned short curRed = 255;
 	unsigned short curGreen = 255;
 	unsigned short curBlue = 255;
+	unsigned short curAlpha = 255;
 	
-	for(int x = 0; x < 256; x++){
-		for(int y = 0; y < 256; y++){
-			SDL_SetRenderDrawColor(renderer, curRed, curGreen, curBlue, 255);
-			SDL_RenderDrawPoint(renderer, x, y);
-			if(curRed   > 0) curRed--;
-			if(curGreen > 0) curGreen--;
-			if(curBlue  > 0) curBlue--;
-		}
+	SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+	
+	curRed = 255;
+	curGreen = 0;
+	curBlue = 0;
+	
+	// current color
+	SDL_SetRenderDrawColor(renderer, curRed, curGreen, curBlue, 255);
+	SDL_Rect square = {0, 0, 255, 255};
+	SDL_RenderFillRect(renderer, &square);
+	
+	curRed = 255;
+	curGreen = 255;
+	curBlue = 255;
+	
+	// white
+	for(int i = 0; i < 128; i++){
+		SDL_SetRenderDrawColor(renderer, curRed, curGreen, curBlue, curAlpha);
+		SDL_RenderDrawLine(renderer, i, 0, i, 255);
 		
-		curRed = 255;
-		curGreen = 255;
-		curBlue = 255;
+		if(curRed   > 0) curRed--;
+		if(curGreen > 0) curGreen--;
+		if(curBlue  > 0) curBlue--;
+		if(curAlpha > 0) curAlpha -= 2;
 	}
+	
+	curRed = 0;
+	curGreen = 0;
+	curBlue = 0;
+	curAlpha = 255;
+	
+	// black
+	for(int i = 255; i > 128; i--){
+		SDL_SetRenderDrawColor(renderer, curRed, curGreen, curBlue, curAlpha);
+		SDL_RenderDrawLine(renderer, 0, i, 255, i);
+		
+		if(curRed   < 255) curRed += 0.5;
+		if(curGreen < 255) curGreen += 0.5;
+		if(curBlue  < 255) curBlue += 0.5;
+		if(curAlpha > 0) curAlpha -= 2;
+	}
+	
+	SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_NONE);
 	
 	SDL_RenderPresent(renderer);
 
